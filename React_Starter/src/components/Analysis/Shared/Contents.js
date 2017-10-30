c184d8039576dfc376b328ae96b5400ee72fa193
yongdol <kungbuk77@gmail.com>
2017-10-30 14:18:33 +0900

    fix indent 4 => 2

--- React_Starter/src/components/Analysis/Shared/Contents.js
+++ React_Starter/src/components/Analysis/Shared/Contents.js
@@ -4,45 +4,45 @@ import GraphContent from "./GraphContent";
 import TableContent from "./TableContent";
 
 class Contents extends Component {
-    render() {
-        const { obj, scheme, ind, job_id } = this.props;
-        const gridNum = 12/scheme[ind].length;
-        switch (obj.form) {
-            case "markdown" :
-                return (
-                    <MarkdownContent
-                        obj={obj}
-                        grid={gridNum}
-                        i={ind}
-                    />
-                );
+  render() {
+    const {obj, scheme, ind, job_id} = this.props;
+    const gridNum = 12 / scheme[ind].length;
+    switch (obj.form) {
+      case "markdown" :
+        return (
+          <MarkdownContent
+            obj={obj}
+            grid={gridNum}
+            i={ind}
+          />
+        );
 
-            case "graph" :
-                return (
-                    <GraphContent
-                        obj={obj}
-                        grid={gridNum}
-                        i={ind}
-                    />
-                );
+      case "graph" :
+        return (
+          <GraphContent
+            obj={obj}
+            grid={gridNum}
+            i={ind}
+          />
+        );
 
-            case "table" :
-                return (
-                    <TableContent
-                        obj={obj}
-                        grid={gridNum}
-                        i={ind}
-                        job_id={job_id}
-                    />
-                );
-            default :
-                return (
-                    <div>
-                        Error!
-                    </div>
-                )
-        }
+      case "table" :
+        return (
+          <TableContent
+            obj={obj}
+            grid={gridNum}
+            i={ind}
+            job_id={job_id}
+          />
+        );
+      default :
+        return (
+          <div>
+            Error!
+          </div>
+        )
     }
+  }
 }
 
 
--- React_Starter/src/components/Dashboard/SummarySampleRender/SampleDashboard.js
+++ React_Starter/src/components/Dashboard/SummarySampleRender/SampleDashboard.js
@@ -8,85 +8,84 @@ import {isEmpty} from "lodash";
 import {BeatLoader} from "react-spinners";
 
 
-
 class SampleDashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null,
-            render_error: ""
-        };
-        this.pmList = this.pmList.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      render_error: ""
+    };
+    this.pmList = this.pmList.bind(this);
+  }
 
-    componentWillMount() {
-        this.pmList();
-        // console.log('token', token );
-    }
+  componentWillMount() {
+    this.pmList();
+    // console.log('token', token );
+  }
 
-    pmList() {
-        const locale = this.props.i18n.language;
-        return axios.get(BACKEND_URL + "/overview_sample_report", {
-            params: {
-                locale: locale
-            }
-        }).then((res) => {
-            if (res.data.e_msg.status === 200) {
-                this.setState({data: res.data.data})
-            } else {
-                const result = res.data.e_msg.message;
-                this.setState({render_error: result});
-            }
-        }).catch((res) => {
-            console.log('res', res);
-        });
-    }
+  pmList() {
+    const locale = this.props.i18n.language;
+    return axios.get(BACKEND_URL + "/overview_sample_report", {
+      params: {
+        locale: locale
+      }
+    }).then((res) => {
+      if (res.data.e_msg.status === 200) {
+        this.setState({data: res.data.data})
+      } else {
+        const result = res.data.e_msg.message;
+        this.setState({render_error: result});
+      }
+    }).catch((res) => {
+      console.log('res', res);
+    });
+  }
 
-    render() {
-        const style = {
-            center : {
-                margin : 'auto',
-                paddingTop: '200px'
-            }
-        };
-        return (
-            <div className="animated fadeIn">
-                <Row>
-                    {
-                        isEmpty(this.state.data) ? (
-                            <div className="sweet-loading" style={style.center}>
-                                <BeatLoader
-                                    color={'#4A90E2'}
-                                    loading={true}
-                                    size={20}
-                                />
-                            </div>
-                        ) : this.state.data.map((item, i) => {
-                            const job_id = item.id;
-                            const service_id = item.service_id;
-                            const service_name = item.service_name;
-                            const service_brief = item.service_brief;
-                            const report_name = item.report_name;
-                            const json_data = JSON.parse(item.sum_json);
-                            const sum_info = json_data.summary_info;
-                            const sum_graph = json_data.summary_graph;
-                            return <SummarySampleRender
-                                key={i}
-                                job_id={job_id}
-                                service_id={service_id}
-                                service_name={service_name}
-                                service_brief={service_brief}
-                                report_name={report_name}
-                                info={sum_info}
-                                graph={sum_graph}
-                                history={this.props.history}
-                            />
-                        })
-                    }
-                </Row>
-            </div>
-        )
-    }
+  render() {
+    const style = {
+      center: {
+        margin: 'auto',
+        paddingTop: '200px'
+      }
+    };
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          {
+            isEmpty(this.state.data) ? (
+              <div className="sweet-loading" style={style.center}>
+                <BeatLoader
+                  color={'#4A90E2'}
+                  loading={true}
+                  size={20}
+                />
+              </div>
+            ) : this.state.data.map((item, i) => {
+              const job_id = item.id;
+              const service_id = item.service_id;
+              const service_name = item.service_name;
+              const service_brief = item.service_brief;
+              const report_name = item.report_name;
+              const json_data = JSON.parse(item.sum_json);
+              const sum_info = json_data.summary_info;
+              const sum_graph = json_data.summary_graph;
+              return <SummarySampleRender
+                key={i}
+                job_id={job_id}
+                service_id={service_id}
+                service_name={service_name}
+                service_brief={service_brief}
+                report_name={report_name}
+                info={sum_info}
+                graph={sum_graph}
+                history={this.props.history}
+              />
+            })
+          }
+        </Row>
+      </div>
+    )
+  }
 
 }
 
